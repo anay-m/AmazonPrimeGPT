322e3863aa629178f8fd0ac74e6e40b904968887
Anant Garg <anantgar@umich.edu>
2023-11-19 00:31:37 -0500

    PineCone Functionality

---
+++ upsert_to_pc.py
@@ -0,0 +1,188 @@
+import pinecone
+import requests
+import os
+import json
+from pprint import pprint
+from openai import OpenAI
+from dotenv import load_dotenv
+load_dotenv()
+client = OpenAI()
+
+api_key = os.environ.get('RAINFOREST_KEY')
+
+pinecone.init(api_key=os.environ.get('PINECONE'), environment="gcp-starter")
+
+def fetch_product_data(url):
+    params = {
+        'api_key' : api_key,
+        'type': 'product',
+        'url' : url,
+        'include_summarization_attributes' : 'true'
+    }
+    response = requests.get("https://api.rainforestapi.com/request", params)
+    return response.json()
+
+
+# Function to fetch Q&A using URL
+def fetch_questions(asin):
+    params = {
+        'api_key' : api_key,
+        'type' : 'questions',
+        'url' : 'https://www.amazon.com/ask/questions/asin/' + asin + '/',
+        'max_page' : 1,
+        'sort_by' : 'most_helpful'
+    }
+    response = requests.get("https://api.rainforestapi.com/request", params)
+    return response.json()
+
+
+def fetch_all_reviews(url):
+    params = {
+        'api_key' : api_key,
+        'type' : 'reviews',
+        'url' : url + "/product-reviews/",
+        'output' : 'json',
+        'max_page' : 1,
+        'sort_by' : 'most_recent',
+        'global_reviews' : 'false'
+    }
+
+    response = requests.get("https://api.rainforestapi.com/request", params)
+    return response.json()
+
+
+def format_product(url):
+    # file_path = "dollhouse.json"
+    # with open(file_path, 'r') as json_file:
+    #     doll = json.load(json_file)
+    doll = fetch_product_data(url)
+
+    if doll["product"].get("feature_bullets") is not None:
+        features = " ".join(doll["product"]["feature_bullets"])
+        response = client.embeddings.create(
+            input="Title: " + doll["product"]["title"] + "\n" + "Description: " + features + "\n" + "Specifications: " + doll["product"]["specifications_flat"],
+            model="text-embedding-ada-002"
+        )
+
+        vec = {
+            'id' : doll["product"]["asin"] + '-P',
+            'values' : response.data[0].embedding,
+            'metadata' : {
+                'asin' : doll["product"]["asin"],
+                'title' : doll["product"]["title"],
+                'description' : features,
+                'price' : doll["product"]["buybox_winner"]["price"]["value"],
+                'rating' : doll["product"]["rating"],
+                'specifications' : doll["product"]["specifications_flat"],
+                'summarization_attributes' : [att["name"] + ": " + str(att["value"]) + "/5.0" for att in doll["product"]["summarization_attributes"]]
+            }
+        }
+    else:
+        response = client.embeddings.create(
+            input="Title: " + doll["product"]["title"] + "\n" + "Specifications: " + doll["product"]["specifications_flat"],
+            model="text-embedding-ada-002"
+        )
+
+        vec = {
+            'id' : doll["product"]["asin"] + '-P',
+            'values' : response.data[0].embedding,
+            'metadata' : {
+                'asin' : doll["product"]["asin"],
+                'title' : doll["product"]["title"],
+                'description' : "",
+                'price' : doll["product"]["buybox_winner"]["price"]["value"],
+                'rating' : doll["product"]["rating"],
+                'specifications' : doll["product"]["specifications_flat"],
+                'summarization_attributes' : [att["name"] + ": " + str(att["value"]) + "/5.0" for att in doll["product"]["summarization_attributes"]]
+            }
+        }
+    # pprint(vec)
+    return vec
+
+def format_reviews(url, asin):
+    # file_path = "review_result_lebron.json"
+    # with open(file_path, 'r') as json_file:
+    #     doll = json.load(json_file)
+    doll = fetch_all_reviews(url)
+    vectors = []
+    
+    for v in doll["reviews"]:
+        response = client.embeddings.create(
+            input="Title: " + v["title"] + "\n" + "Review Body: " + v["body"],
+            model="text-embedding-ada-002"
+        )
+        vec = {
+            'id' : asin + "-" + str(v["page"]) + "-" + str(v["position"]) + "-R",
+            'values' : response.data[0].embedding,
+            'metadata' : {
+                'asin' : asin,
+                'title' : v["title"],
+                'body' : v["body"],
+                'rating' : v["rating"],
+            }
+        }
+
+        vectors.append(vec)
+
+    # pprint(vectors)
+    return vectors
+
+def format_qa(asin):
+    # file_path = "qa_result.json"
+    # with open(file_path, 'r') as json_file:
+    #     doll = json.load(json_file)
+    doll = fetch_questions(asin)
+
+    vectors = []
+    
+    for v in doll["questions"]:
+        if v.get("answer") is not None:
+            response = client.embeddings.create(
+                input="Question: " + v["question"] + "\n" + "Answer: " + v["answer"],
+                model="text-embedding-ada-002"
+            )
+
+            vec = {
+                'id' : doll["product"]["asin"] + "-" + str(v["page"]) + "-" + str(v["position"]) + "-Q",
+                'values' : response.data[0].embedding,
+                'metadata' : {
+                    'asin' : doll["product"]["asin"],
+                    'question' : v["question"],
+                    'answer' : v["answer"],
+                }
+            }
+
+        vectors.append(vec)
+
+    # pprint(vectors)
+    return vectors
+
+def checkForAsin(asin):
+    vectorx = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    value_to_check = asin
+    data = index.query(
+        vector= vectorx,
+        filter={
+            'ASIN': value_to_check
+        },
+        top_k=1,
+        include_metadata=True
+    )
+    if not data['matches']:
+        return False
+    else:
+        return True
+
+def main(data):
+    #data = request.get_json()
+    url = data.get('URL')
+    asin = data.get("ASIN")
+    index = pinecone.Index("gptprime")
+    if checkForAsin(asin=asin) is False:
+        vectors = format_qa(asin)
+        vectors.extend(format_reviews(url, asin))
+        vectors.append(format_product(url))
+
+        index.upsert(vectors)
+
+    return {}
